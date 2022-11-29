6f79bd43b5ce355edf6c8c6cfe459badf8c500ce
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-11-29 11:06:24 +0300

    Add NavBar component

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/reservations">My Reservations</NavLink>
+        </li>
+        <li>
+          {/* <NavLink to="/bikes">Bikes</NavLink> */}
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
