73caebf7ff598304467e3e923f63be80ff49d3b4
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-11-29 15:04:19 +0300

    Add async actions

--- src/redux/reservations/actions/addReservation.js
+++ src/redux/reservations/actions/addReservation.js
@@ -1,25 +1,19 @@
 // acction types
-const CALL_API_REQUEST = "CALL_API_REQUEST"
-const ADD_RESERVATION_SUCCESS = "ADD_RESERVATION_SUCCESS"
-const ADD_RESERVATION_FAILURE = "ADD_RESERVATION_FAILURE"
+const CALL_API_REQUEST = 'CALL_API_REQUEST';
+const ADD_RESERVATION_SUCCESS = 'ADD_RESERVATION_SUCCESS';
+const ADD_RESERVATION_FAILURE = 'ADD_RESERVATION_FAILURE';
 
 // action creators
-const addReservationsRequest = () => {
-    return {
-        type: CALL_API_REQUEST
-    }
-}
+const addReservationsRequest = () => ({
+  type: CALL_API_REQUEST,
+});
 
-const addReservationsSuccess = (bike) => {
-    return {
-        type: ADD_RESERVATION_SUCCESS,
-        payload: bike
-    }
-}
+const addReservationsSuccess = (bike) => ({
+  type: ADD_RESERVATION_SUCCESS,
+  payload: bike,
+});
 
-const addReservationsFailure = (error) => {
-    return {
-        type: ADD_RESERVATION_FAILURE,
-        payload: error
-    }
-}
+const addReservationsFailure = (error) => ({
+  type: ADD_RESERVATION_FAILURE,
+  payload: error,
+});
--- src/redux/reservations/actions/removeFromReservation.js
+++ src/redux/reservations/actions/removeFromReservation.js
@@ -1,25 +1,19 @@
 // acction types
-const CALL_API_REQUEST = "CALL_API_REQUEST"
-const REMOVE_RESERVATION_SUCCESS = "REMOVE_RESERVATION_SUCCESS"
-const REMOVE_RESERVATION_FAILURE = "REMOVE_RESERVATION_FAILURE"
+const CALL_API_REQUEST = 'CALL_API_REQUEST';
+const REMOVE_RESERVATION_SUCCESS = 'REMOVE_RESERVATION_SUCCESS';
+const REMOVE_RESERVATION_FAILURE = 'REMOVE_RESERVATION_FAILURE';
 
 // action creators
-const removeReservationsRequest = () => {
-    return {
-        type: CALL_API_REQUEST
-    }
-}
+const removeReservationsRequest = () => ({
+  type: CALL_API_REQUEST,
+});
 
-const removeReservationsSuccess = (bike) => {
-    return {
-        type: REMOVE_RESERVATION_SUCCESS,
-        payload: bike
-    }
-}
+const removeReservationsSuccess = (bike) => ({
+  type: REMOVE_RESERVATION_SUCCESS,
+  payload: bike,
+});
 
-const removeReservationsFailure = (error) => {
-    return {
-        type: REMOVE_RESERVATION_FAILURE,
-        payload: error
-    }
-}
+const removeReservationsFailure = (error) => ({
+  type: REMOVE_RESERVATION_FAILURE,
+  payload: error,
+});
