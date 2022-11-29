26d516db0768352625ff51b114406abb8506e13b
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-11-29 13:57:26 +0300

    Create Initila state for add reservations

--- src/redux/reservations/actions/addReservation.js
+++ src/redux/reservations/actions/addReservation.js
@@ -3,3 +3,9 @@ const CALL_API_REQUEST = "CALL_API_REQUEST"
 const ADD_RESERVATION_SUCCESS = "ADD_RESERVATION_SUCCESS"
 const ADD_RESERVATION_FAILURE = "ADD_RESERVATION_FAILURE"
 
+// action creators
+const initialState = {
+    loading: false,
+    data: [],
+    error: '',
+};
