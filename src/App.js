9fdc40b0cc08ae659e50aa90d7d19100020a5192
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-11-30 11:04:14 +0300

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './components/home/home';
 import Reservations from './components/reservations/reservations';
 import NavBar from './components/navbar/navbar';
+import Form from './components/reservations/reserveForm';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/reservations" element={<Reservations />} />
-        {/* <Route path="/bikes" element={<Bikes/>} /> */}
+        <Route path="/form" element={<Form/>} />
       </Routes>
     </BrowserRouter>
 
