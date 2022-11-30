390f793564f49f48cc8e70b9716e392e2d5abf0a
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-11-30 22:47:05 +0300

    Add Redux dev tool enhancer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reservations/reducers/configureStore';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
