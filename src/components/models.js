e754eceb45c53109d395db2f8ceaeee6c005f053
jevazquezb <je.vazquezb@gmail.com>
2022-12-01 16:56:08 -0600

    Add the models component

--- src/components/models.js
+++ src/components/models.js
@@ -1,7 +1,105 @@
-function Models() {
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Icon } from '@iconify/react';
+import { fetchModelsAsync } from '../redux/models/models';
+import '../styles/models.css';
+
+function Bike({ bike }) {
+  const {
+    id, brand, model, photo, description,
+  } = bike;
+
+  const socialMedia = () => {
+    const modelMedia = ['ri:facebook-fill', 'mdi:twitter', 'mdi:instagram'];
+    const social = [];
+
+    for (let i = 0; i < modelMedia.length; i += 1) {
+      social.push(
+        <li className="social-cont" key={modelMedia[i]}>
+          <Icon
+            className="social-mini"
+            color="#bbbbbb"
+            icon={modelMedia[i]}
+          />
+        </li>,
+      );
+    }
+    return social;
+  };
+
   return (
-    <h1>Models</h1>
+    <li className="bike-item">
+      <Link className="bike-link" to={`/models/${id}`}>
+        <div className="img-cont">
+          <div className="circle" />
+          <img
+            src={photo}
+            alt={`${brand} ${model}`}
+            title={`${brand} ${model}`}
+            className="img-bike"
+          />
+        </div>
+        <h2 className="model-name">{model}</h2>
+      </Link>
+      <p className="bike-description">{description}</p>
+      <ul className="bike-social">
+        { socialMedia() }
+      </ul>
+    </li>
   );
 }
+
+function BikesList() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchModelsAsync());
+  }, [dispatch]);
+
+  const bikes = useSelector((state) => state.models);
+
+  const createList = (bikes) => {
+    const list = bikes.map((bike) => (
+      <Bike
+        bike={bike}
+        key={bike.id}
+      />
+    ));
+    return list;
+  };
+
+  return (
+    <ul className="bikes-list">
+      { createList(bikes) }
+    </ul>
+  );
+}
+
+function Models() {
+  return (
+    <>
+      <h1 className="models-title">LATEST MODELS</h1>
+      <p className="model-subtitle">Please select a Ducati Model</p>
+      <BikesList />
+    </>
+  );
+}
+
+Bike.propTypes = {
+  bike: PropTypes.shape({
+    id: PropTypes.number,
+    brand: PropTypes.string,
+    model: PropTypes.string,
+    photo: PropTypes.string,
+    power: PropTypes.number,
+    weight: PropTypes.number,
+    price: PropTypes.number,
+    created_at: PropTypes.string,
+    updated_at: PropTypes.string,
+    description: PropTypes.string,
+  }).isRequired,
+};
 
 export default Models;
