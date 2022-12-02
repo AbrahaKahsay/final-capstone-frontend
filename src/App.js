1ef5710a6d9d309dbbb7693786b692a96eb3aca6
jevazquezb <je.vazquezb@gmail.com>
2022-12-02 14:53:49 -0600

    Add ModelDetails component to routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import NavDrawer from './components/nav_drawer';
 import Models from './components/models';
+import ModelDetails from './components/model_details';
 import Reserve from './components/reserve';
 import MyReservations from './components/my_reservations';
 import AddMotorcycle from './components/add_motorcycle';
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Models />} />
           <Route path="/models" element={<Models />} />
+          <Route path="/models/:id" element={<ModelDetails />} />
           <Route path="/reserve" element={<Reserve />} />
           <Route path="/my-reservations" element={<MyReservations />} />
           <Route path="/add-motorcycle" element={<AddMotorcycle />} />
