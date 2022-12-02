278bbf8144264ebc59ce71c789515eca7e3813bc
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-12-03 01:04:09 +0300

    Modify my-reservations for start/end dates

--- src/components/my_reservations.js
+++ src/components/my_reservations.js
@@ -4,22 +4,31 @@ import { fetchReservationsFromServer } from '../redux/reservations/reservations'
 
 function MyReservations() {
   const dispatch = useDispatch();
-  const reservations = useSelector((state) => state.reservations);
-
+  const reservations = useSelector((state) => state.reservations.reservations);
+  console.log(reservations);
   useEffect(() => {
     dispatch(fetchReservationsFromServer);
   }, [dispatch]);
 
   return (
     <div>
-      <h1>Hi</h1>
-      <h2>{reservations}</h2>
-      {/* {reservations.map((reservation) => (
+      <h1>This is a list of your reservations</h1>
+      {reservations.map((reservation) => (
         <div key={reservation.id}>
-          <h2>{reservation.location}</h2>
-          <h2>{reservation.date}</h2>
+          <h2>
+            City:
+            {reservation.location}
+          </h2>
+          <h2>
+            Start Date:
+            {reservation.start_date}
+          </h2>
+          <h2>
+            End Date:
+            {reservation.end_date}
+          </h2>
         </div>
-      ))} */}
+      ))}
 
     </div>
   );
