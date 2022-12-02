65872996d986207b0475bb23c84770ffa4fe925f
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-12-02 18:28:21 +0300

    Add a reservation form

--- src/components/my_reservations.js
+++ src/components/my_reservations.js
@@ -1,6 +1,27 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchReservationsFromServer } from '../redux/reservations/reservations';
+
 function MyReservations() {
+  const dispatch = useDispatch();
+  const reservations = useSelector((state) => state.reservations);
+
+  useEffect(() => {
+    dispatch(fetchReservationsFromServer);
+  }, [dispatch]);
+
   return (
-    <h1>My Reservations</h1>
+    <div>
+      <h1>Hi</h1>
+      <h2>{reservations}</h2>
+      {/* {reservations.map((reservation) => (
+        <div key={reservation.id}>
+          <h2>{reservation.location}</h2>
+          <h2>{reservation.date}</h2>
+        </div>
+      ))} */}
+
+    </div>
   );
 }
 
