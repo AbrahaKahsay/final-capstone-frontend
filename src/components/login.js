3bd44b1415d4a6b360df4543da0b3706bdad41cd
Aiziji <aletv1501@gmail.com>
2022-12-02 20:29:53 -0400

    Add redu to login page

--- src/components/login.js
+++ src/components/login.js
@@ -1,21 +1,45 @@
 import '../styles/login.css';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { findUserAsync } from '../redux/models/login';
+//  error = This user is not in database
 
 const Login = () => {
+  const navigate = useNavigate();
+  const currentUser = useSelector((state) => state.current_user);
+  const [name, setName] = useState('');
+  const dispatch = useDispatch();
   const changeLogin = () => {
     document.getElementById('signup').classList.toggle('inactive');
     document.getElementById('login').classList.toggle('inactive');
   };
+  const handleOnChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    dispatch(findUserAsync(name));
+  };
+
+  useEffect(() => {
+    console.log(currentUser);
+    if (currentUser !== 'This user is not in database') {
+      navigate('/home');
+    }
+  });
   return (
     <section id="login" className="main-authentification">
       <div className="container">
         <div className="screen">
           <div className="screen__content">
             <h2 id="authentification-h2">Log in</h2>
-            <form className="login">
+            <form className="login" onSubmit={handleOnSubmit}>
               <div className="login__field">
-                <input type="text" className="login__input" placeholder="User name" />
+                <input type="text" onChange={handleOnChange} className="login__input" placeholder="User name" />
               </div>
-              <button className="button login__submit" type="button">
+              <button className="button login__submit" type="submit">
                 <span className="button__text">Log In Now</span>
               </button>
               <button className="button login__submit" type="button" onClick={changeLogin}>
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import modelReducer from './models/models';
+import userReducer from './models/login';
 
 const store = configureStore({
   reducer: {
     models: modelReducer,
+    current_user: userReducer,
   },
 });
 
