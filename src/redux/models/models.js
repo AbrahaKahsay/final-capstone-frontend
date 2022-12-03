350bc87dd4696117940086e7f3a790a65a4f23d7
jevazquezb <je.vazquezb@gmail.com>
2022-12-03 13:47:02 -0600

    Add addBikeAsync function and add it to model slice

--- src/redux/models/models.js
+++ src/redux/models/models.js
@@ -2,6 +2,7 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 // Actions
 const FETCH_BIKES_MODELS = 'final_capstone_frontend/models/FETCH_BIKES_MODELS';
+const ADD_BIKE_MODEL = 'final_capstone_frontend/models/ADD_BIKE_MODEL';
 
 // URL
 const modelsURL = 'http://localhost:3001/api/v1/bikes';
@@ -16,19 +17,38 @@ const fetchModelsAsync = createAsyncThunk(
   },
 );
 
+const addBikeAsync = createAsyncThunk(
+  ADD_BIKE_MODEL,
+  async (bike) => {
+    await fetch(modelsURL, {
+      method: 'POST',
+      body: JSON.stringify(bike),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+    return bike;
+  },
+);
+
 // Reducer
 const initialState = [];
 
 const modelSlice = createSlice({
   name: 'models',
   initialState,
-  extraReducers: {
-    [fetchModelsAsync.fulfilled]: (state, action) => (
-      [...action.payload]
-    ),
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchModelsAsync.fulfilled, (state, action) => (
+        [...action.payload]
+      ))
+      .addCase(addBikeAsync.fulfilled, (state, action) => (
+        [...state, { ...action.payload }]
+      ));
   },
 });
 
-export { fetchModelsAsync };
+export { fetchModelsAsync, addBikeAsync };
 
 export default modelSlice.reducer;
