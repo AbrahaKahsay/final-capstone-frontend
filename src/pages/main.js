ba73b1d5bf3eb38a2de555d35c1a6b33665f061c
jevazquezb <je.vazquezb@gmail.com>
2022-12-03 13:05:59 -0600

    Add models/:id route and ModelDetails component

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import NavDrawer from '../components/nav_drawer';
 import Models from '../components/models';
+import ModelDetails from '../components/model_details';
 import Reserve from '../components/reserve';
 import MyReservations from '../components/my_reservations';
 import AddMotorcycle from '../components/add_motorcycle';
@@ -13,6 +14,7 @@ function Main() {
       <main>
         <Routes>
           <Route path="/models" element={<Models />} />
+          <Route path="/models/:id" element={<ModelDetails />} />
           <Route path="/reserve" element={<Reserve />} />
           <Route path="/my-reservations" element={<MyReservations />} />
           <Route path="/add-motorcycle" element={<AddMotorcycle />} />
