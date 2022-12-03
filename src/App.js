4cb8e3ec4da3083bd5b36b59bf195bcb03b6c63f
Aiziji <aletv1501@gmail.com>
2022-12-02 23:10:19 -0400

    Fix login and logout

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/home" element={<Main />} />
+        <Route path="/home/*" element={<Main />} />
         <Route path="/" element={<Authentification />} />
       </Routes>
     </>
