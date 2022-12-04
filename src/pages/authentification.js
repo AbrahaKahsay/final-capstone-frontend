dfccc82d8b2c77a79a4c80e79c4baa88f56abf8c
Aiziji <aletv1501@gmail.com>
2022-12-04 17:18:33 -0400

    Delete unused variables and console.logs

--- src/pages/authentification.js
+++ src/pages/authentification.js
@@ -1,10 +1,7 @@
-import { useSelector } from 'react-redux';
 import Login from '../components/login';
 import Signin from '../components/singin';
 
 function Authentification() {
-  const user = useSelector((state) => state.current_user);
-  console.log(user);
   return (
     <>
       <Login />
