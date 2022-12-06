ff12193d5493307877af9c2c77fb4a15d7133e63
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-12-06 16:04:01 +0300

    Modify reserve.js

--- src/components/reserve.js
+++ src/components/reserve.js
@@ -2,12 +2,16 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { addReservation } from '../redux/reservations/reservations';
-// import { postReservations } from '../../redux/reservations/newAxios';
+import '../styles/reserve.css'
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 
 const Reserve = () => {
   const dispatch = useDispatch();
-  //   const reservations = useSelector((state) => state.reservations.reservations);
-
+  // const {bike_id} = useParams()
+  // const bike = useSelector((state) => state.models.find((bike)=> bike.id === bike_id))
+  // const user = useSelector((state) => state.current_user)
   useEffect(() => {
     dispatch(addReservation);
   }, [dispatch]);
@@ -15,11 +19,15 @@ const Reserve = () => {
   const [start_date, setStartDate] = useState('');
   const [end_date, setEndDate] = useState('');
   const [location, setLocation] = useState('');
+  // const [model, setModel] = useState('')
+  
   const [bike_id, setBikeId] = useState(1);
   const [user_id, setUserId] = useState(1);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // const bike_id = model.id
+    // const current_user = current_user
 
     const formData = {
       user_id, bike_id, start_date, end_date, location,
@@ -32,10 +40,12 @@ const Reserve = () => {
   };
 
   return (
+    <div className='form-container'>
     <form onSubmit={handleSubmit}>
       {/* <div>
         <label>Select Bike</label>
         <input type="text" value={bike.model} />
+        onChange={(e) => { setModel(e.target.value); }}
       </div> */}
 
       <div>
@@ -75,7 +85,7 @@ const Reserve = () => {
       </div>
 
       <div>
-        <label>User</label>
+        <label>User name</label>
         <input
           type="number"
           value={user_id}
@@ -83,12 +93,13 @@ const Reserve = () => {
         />
       </div>
 
-      {/* //   <div>
-    //     <label>User name</label>
-    //     <input type="text" value={user.name} />
-    //   </div> */}
-      <input type="submit" value="Add Reservation" />
+      {/* <div>
+        <label>User name</label>
+        <input type="text" value={user.name} />
+      </div> */}
+      <input className='reserve-btn' type="submit" value="Add Reservation" />
     </form>
+    </div>
   );
 };
 
