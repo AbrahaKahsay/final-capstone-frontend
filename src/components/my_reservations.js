4da720af2194c438bab91d7af0dee7001e4d7bd3
Aiziji <aletv1501@gmail.com>
2022-12-07 14:32:47 -0400

    Fix add reservtions

--- src/components/my_reservations.js
+++ src/components/my_reservations.js
@@ -1,16 +1,20 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchReservationsFromServer, removeReservations } from '../redux/reservations/reservations';
+import { fetchModelsAsync } from '../redux/models/models';
 import '../styles/my_reservations.css';
 
 function MyReservations() {
   const dispatch = useDispatch();
   const reservations = useSelector((state) => state.reservations.reservations);
-  console.log(reservations);
+  const bikes = useSelector((state) => state.models);
+  console.log('bikes');
+  console.log(bikes);
 
   useEffect(() => {
-    dispatch(fetchReservationsFromServer);
-  }, [dispatch]);
+    dispatch(fetchReservationsFromServer());
+    dispatch(fetchModelsAsync());
+  });
 
   const handleDelete = () => {
     dispatch(removeReservations);
@@ -19,25 +23,32 @@ function MyReservations() {
   return (
     <div className="res-container">
       <h1>My Reservations</h1>
-      {reservations.map((reservation) => (
-        <div className="card" key={reservation.id}>
-          <h2>
-            City:
-            {reservation.location}
-          </h2>
-          <h2>
-            Start Date:
-            {reservation.start_date}
-          </h2>
-          <h2>
-            End Date:
-            {reservation.end_date}
-          </h2>
-          <div className="delete-reservation">
-            <button type="button" onClick={handleDelete}>Delete</button>
+      {reservations.map((reservation) => {
+        const bike = bikes.find((bike) => bike.id === reservation.bike_id);
+        return (
+          <div className="card" key={reservation.id}>
+            <h2>
+              City:
+              {reservation.location}
+            </h2>
+            <h2>
+              bike:
+              {bike.model}
+            </h2>
+            <h2>
+              Start Date:
+              {reservation.start_date}
+            </h2>
+            <h2>
+              End Date:
+              {reservation.end_date}
+            </h2>
+            <div className="delete-reservation">
+              <button type="button" onClick={handleDelete}>Delete</button>
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
