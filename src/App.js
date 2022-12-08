4238026318f708ed964b9a0e86d5b0de0b1ef315
lucascr <lcrodrigues1995@gmail.com>
2022-12-08 18:59:26 -0300

    Fix linters

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 import Main from './pages/main';
 import Authentification from './pages/authentification';
 
-const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Authentification />} />
-        <Route path="/home/*" element={<Main />} />
-      </Routes>
-    </>
-  );
-}
+const App = () => (
+  <>
+    <Routes>
+      <Route path="/" element={<Authentification />} />
+      <Route path="/home/*" element={<Main />} />
+    </Routes>
+  </>
+);
 
 export default App;
